5e89cb989cfca48669a8ff0c0442df468c156149
Sidney Cadot <sidney@jigsaw.nl>
2014-06-10 19:02:32 +0200

    Added shell script to download sample scenes from the Mitsuba web site.

---
+++ sample-scenes/download.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+wget http://www.mitsuba-renderer.org/scenes/cbox.zip
+wget http://www.mitsuba-renderer.org/scenes/medieval.zip
+wget http://www.mitsuba-renderer.org/scenes/veach_mi.zip
+wget http://www.mitsuba-renderer.org/scenes/veach_bidir.zip
+wget http://www.mitsuba-renderer.org/scenes/torus.zip
+wget http://www.mitsuba-renderer.org/scenes/matpreview.zip
+wget http://www.mitsuba-renderer.org/scenes/hetvol.zip
+wget http://www.mitsuba-renderer.org/scenes/sponza.zip
+wget http://www.mitsuba-renderer.org/scenes/cbox_sss.zip
+wget http://www.mitsuba-renderer.org/scenes/sssdragon.zip
+wget http://www.mitsuba-renderer.org/scenes/irawan.zip
+wget http://www.mitsuba-renderer.org/scenes/scarf.zip
