c10049565583776a38c5d3fe7598dc4d331aa217
Sidney Cadot <sidney@jigsaw.nl>
2014-07-09 00:05:26 +0200

    Fixed path

--- sidney/StartMitsubaTmux.sh
+++ sidney/StartMitsubaTmux.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-exec tmux new-session -s mitsuba "source $HOME/mitsuba/mitsuba-current/setpath.sh ; while true ; do mtssrv ; sleep 20 ; done"
+exec tmux new-session -s mitsuba "source $HOME/Repositories/github/MitsubaARCF/sidney/mitsuba-current/setpath.sh ; while true ; do mtssrv ; sleep 20 ; done"
