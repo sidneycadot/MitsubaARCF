9847774c43846f3a8ec357eacc2c965e60edab9d
Sidney Cadot <sidney@jigsaw.nl>
2014-07-21 08:29:38 +0200

    Updated integrator invocations to make sure we get pictures for all of them.

--- arcf-model/check-integrators.sh
+++ arcf-model/check-integrators.sh
@@ -2,20 +2,18 @@
 
 rm -rf integrators
 mkdir integrators
-touch integrators/begin
 
-# direct path volpath_simple volpath bdpt photonmapper ppm sppm pssmlt mlt erpt ptracer
+ALL12="direct path volpath_simple volpath bdpt photonmapper ppm sppm pssmlt mlt erpt ptracer"
 
-for integrator in direct path volpath_simple volpath bdpt photonmapper ppm sppm pssmlt mlt erpt ptracer
+for integrator in $ALL12
 do
     if [ $integrator = erpt ] ; then
         samplecount=4
     else
-        samplecount=256
+        samplecount=16
     fi
-
     mitsuba -Dintegrator-type=$integrator -Ddetector.sample_count=$samplecount arcf.xml
     mv arcf.exr integrators/$integrator.exr
 done
 
-montage -label %t integrators/*.exr montage.png
+montage -label %t `echo $ALL12 | tr ' ' '\n' | sed 's/\(.*\)/integrators\/\1.exr/'` montage.png
