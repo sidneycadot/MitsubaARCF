4ca698516e0a646b65f330903b88d37bb8e63750
Sidney Cadot <sidney@jigsaw.nl>
2015-05-20 16:37:57 +0200

    Fixing build scripts.

--- build/patches/patch-mitsuba.sh
+++ build/patches/patch-mitsuba.sh
@@ -22,8 +22,8 @@ cp ${PATCHDIR}/arcf_source.cpp ${MITSUBADIR}/src/emitters
 
 # In the CMake and SCons build files, copy the line for the "area" emitter and add a line that replaces "area" with "arcf_source".
 
-sed -i .orig 's/\(\(.*\)area\(.\+\)area\(\.cpp.*\)\)/\1\n\2arcf_source\3arcf_source\4/' ${MITSUBADIR}/src/emitters/CMakeLists.txt
-sed -i .orig 's/\(\(.*\)area\(.\+\)area\(\.cpp.*\)\)/\1\n\2arcf_source\3arcf_source\4/' ${MITSUBADIR}/src/emitters/SConscript
+sed --in-place=.orig 's/\(\(.*\)area\(.\+\)area\(\.cpp.*\)\)/\1\n\2arcf_source\3arcf_source\4/' ${MITSUBADIR}/src/emitters/CMakeLists.txt
+sed --in-place=.orig 's/\(\(.*\)area\(.\+\)area\(\.cpp.*\)\)/\1\n\2arcf_source\3arcf_source\4/' ${MITSUBADIR}/src/emitters/SConscript
 
 ### Add arcf_collimated_disk emitter.
 
@@ -33,8 +33,8 @@ cp ${PATCHDIR}/arcf_collimated_disk.cpp ${MITSUBADIR}/src/emitters
 
 # In the CMake and SCons build files, copy the line for the "area" emitter and add a line that replaces "area" with "arcf_collimated_disk".
 
-sed -i .orig 's/\(\(.*\)area\(.\+\)area\(\.cpp.*\)\)/\1\n\2arcf_collimated_disk\3arcf_collimated_disk\4/' ${MITSUBADIR}/src/emitters/CMakeLists.txt
-sed -i .orig 's/\(\(.*\)area\(.\+\)area\(\.cpp.*\)\)/\1\n\2arcf_collimated_disk\3arcf_collimated_disk\4/' ${MITSUBADIR}/src/emitters/SConscript
+sed --in-place=.orig 's/\(\(.*\)area\(.\+\)area\(\.cpp.*\)\)/\1\n\2arcf_collimated_disk\3arcf_collimated_disk\4/' ${MITSUBADIR}/src/emitters/CMakeLists.txt
+sed --in-place=.orig 's/\(\(.*\)area\(.\+\)area\(\.cpp.*\)\)/\1\n\2arcf_collimated_disk\3arcf_collimated_disk\4/' ${MITSUBADIR}/src/emitters/SConscript
 
 ### Add arcf_aperture texture.
 
@@ -44,8 +44,8 @@ cp ${PATCHDIR}/arcf_aperture.cpp ${MITSUBADIR}/src/textures
 
 # In the CMake and SCons build files, copy the line for the "checkerboard" texture and add a line that replaces "checkerboard" with "arcf_aperture".
 
-sed -i .orig 's/\(\(.*\)checkerboard\(.\+\)checkerboard\(\.cpp.*\)\)/\1\n\2arcf_aperture\3arcf_aperture\4/' ${MITSUBADIR}/src/textures/CMakeLists.txt
-sed -i .orig 's/\(\(.*\)checkerboard\(.\+\)checkerboard\(\.cpp.*\)\)/\1\n\2arcf_aperture\3arcf_aperture\4/' ${MITSUBADIR}/src/textures/SConscript
+sed --in-place=.orig 's/\(\(.*\)checkerboard\(.\+\)checkerboard\(\.cpp.*\)\)/\1\n\2arcf_aperture\3arcf_aperture\4/' ${MITSUBADIR}/src/textures/CMakeLists.txt
+sed --in-place=.orig 's/\(\(.*\)checkerboard\(.\+\)checkerboard\(\.cpp.*\)\)/\1\n\2arcf_aperture\3arcf_aperture\4/' ${MITSUBADIR}/src/textures/SConscript
 
 ### Add arcf_fieldfocus bsdf.
 
@@ -53,5 +53,5 @@ cp ${PATCHDIR}/arcf_fieldfocus.cpp ${MITSUBADIR}/src/bsdfs
 
 # In the CMake and SCons build files, copy the line for the "null" bsdf and add a line that replaces "null" with "arcf_fieldfocus".
 
-sed -i .orig 's/\(\(.*\)null\(.\+\)null\(\.cpp.*\)\)/\1\n\2arcf_fieldfocus\3arcf_fieldfocus\4/' ${MITSUBADIR}/src/bsdfs/CMakeLists.txt
-sed -i .orig 's/\(\(.*\)null\(.\+\)null\(\.cpp.*\)\)/\1\n\2arcf_fieldfocus\3arcf_fieldfocus\4/' ${MITSUBADIR}/src/bsdfs/SConscript
+sed --in-place=.orig 's/\(\(.*\)null\(.\+\)null\(\.cpp.*\)\)/\1\n\2arcf_fieldfocus\3arcf_fieldfocus\4/' ${MITSUBADIR}/src/bsdfs/CMakeLists.txt
+sed --in-place=.orig 's/\(\(.*\)null\(.\+\)null\(\.cpp.*\)\)/\1\n\2arcf_fieldfocus\3arcf_fieldfocus\4/' ${MITSUBADIR}/src/bsdfs/SConscript
